83d5a5a5656b9abc2b2aff788eec1b450b1a6c96
Ed Chin <edgardochin@gmail.com>
2022-04-18 21:03:44 -0500

    initial smart contract code

--- src/backend/contracts/Agora_contracts.sol
+++ src/backend/contracts/Agora_contracts.sol
@@ -5,5 +5,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Agora is ERC721, Ownable {
-    string public
+    // NFT.storage CID for Audio File Metadata (JSON)
+    string public baseURI = 
+        "https://bafybeihzlnpv7eq5i5utkmc7xeub3gvzszjiqu6jv5rpbdhi42niefb6du.ipfs.nftstorage.link/";
 }
