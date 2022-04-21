54d2970e941fc21229fd04693e1e956241327643
Ed Chin <edgardochin@gmail.com>
2022-04-20 22:51:37 -0500

    finish initial unit testing

--- src/backend/test/Agora.test.js
+++ src/backend/test/Agora.test.js
@@ -1,9 +1,11 @@
-/* ___UNIT TESTS___
+/* __________UNIT TESTS_________
    
+run tests via hardhat :  $npx hardhat test
+
 vid time https://youtu.be/Q_cxytZZdnc?t=2629
 https://www.edureka.co/blog/advanced-javascript-tutorial/
 
-__ Tech Stack & Tools ___
+____ Tech Stack & Tools _____
 WAFFLE : library for writing and testing smart contracts.
          Sweeter, simpler and faster than Truffle.
          Works with ethers-js. 
@@ -17,60 +19,81 @@ MOCHA : feature-rich JavaScript test framework running on Node.js
 
  CHAIjs - Assertion library :  https://www.chaijs.com/
     
-*/
+___________________________________________________________________*/
 
 const { expect } = require("chai")
-// var expect = chai.expect;
 const { ethers } = require("hardhat")
 
 const toWei = (num) => ethers.utils.parseEther(num.toString())
 const fromWei = (num) => ethers.utils.formatEther(num)
 
-describe("Agora", function () {
-    let Agora
+describe("UNIT TEST : Agora Smart Contract", function () {
+    const erc721Name = "Agora"
+    const erc721Symbol = "AGRA"
+    let smartContract
     let deployer, artist, user1, user2, users;
     let royaltyFee = toWei(0.01);
-    let URI = "https://bafybeihzlnpv7eq5i5utkmc7xeub3gvzszjiqu6jv5rpbdhi42niefb6du.ipfs.nftstorage.link/";
-    let prices = [toWei(1), toWei(2), toWei(3), toWei(4), toWei(5), toWei(6), toWei(7), toWei(8),];
+    let dataURI = "https://bafybeihzlnpv7eq5i5utkmc7xeub3gvzszjiqu6jv5rpbdhi42niefb6du.ipfs.nftstorage.link/";
+    let prices = [toWei(1),
+                  toWei(2),
+                  toWei(3),
+                  toWei(4),
+                  toWei(5),
+                  toWei(6),
+                  toWei(7),
+                  toWei(8) ];
     let deploymentFees = toWei(prices.length * 0.01)
-    beforeEach(async function () {
-        // Get the ContractFactory and signers here ...
-        const NFTMarketPlaceFactory = await ethers.getContractFactory("Agora");
+    beforeEach(async () => {
+        // instantiate ContractFactory obj
+        const smartContractName = "Agora"
+        const contractFactory = await ethers.getContractFactory(smartContractName);
+        // Assign first 4 sample wallets(signers) to deployer, artists, users ...
         [deployer, artist, user1, user2, ...users] = await ethers.getSigners();
 
-        // Deploy music nft marketplace contract
-        nftMarketplace = await NFTMarketPlaceFactory.deploy(
-            royaltyFee,
+        // Deploy smart contract to blockchain
+        smartContract = await contractFactory.deploy(
             artist.address,
+            royaltyFee,
             prices,
             { value: deploymentFees }
         );
     });
     
-    describe("Deployment", function () {
-        it("Should track name, symbol, URI", async function () {
-            const nftName = "Agora"
-            const nftSymbol = "AGRA"
-            expect(await nftMarketplace.name()).to.equal(nftName);
-            expect(await nftMarketplace.symbol()).to.equal(nftSymbol);
-            expect(await nftMarketplace.baseURI()).to.equal(URI);
-            expect(await nftMarketplace.royaltyFee()).to.equal(royaltyFee);
-            expect(await nftMarketplace.artist()).to.equal(artist.address);
-        });
-        
-        it("Should mint then list all the audio NFTs", async function () {
-            expect( await nftMarketplace.balanceOf(nftMarketplace.address)).to.equal(8);
-            // get each item in from the marketPlace array then check validity of fields
-            await Promise.all(prices.map(async (i, indx) => {
-                const item = await nftMarketplace.marketItems(indx)
-                expect(item.tokenId).to.equal(indx)
-                expect(item.seller).to.equal(deployer.address)
-                expect(item.price).to.equal(i)
-            }));
+    describe("Successfully deployed", function () {
+
+        it("VERFIED: ERC721 name, symbol are valid", async function () {
+            expect(await smartContract.name()).to.equal(erc721Name);
+            expect(await smartContract.symbol()).to.equal(erc721Symbol);
         });
 
-        it("Ether balance should equal deployment fees", async function () {
-            expect( await ethers.provider.getBalance(nftMarketplace.address)).to.equal(deploymentFees);
+        it("VERFIED: Artist address, royalty fee, and data URI are valid", async function () {
+            expect(await smartContract.dataURI()).to.equal(dataURI);
+            expect(await smartContract.royaltyFee()).to.equal(royaltyFee);
+            expect(await smartContract.artist()).to.equal(artist.address);
+        });
+
+        it("VERFIED: Number of initial audio NFTs = 8", async function () {
+            expect( await smartContract.balanceOf(smartContract.address)).to.equal(8);
+            // get each item in from the marketPlace array then check validity of fields
+            // await Promise.all(prices.map(async (i, indx) => {
+            //     const item = await nftM.marketItems(indx)
+            //     expect(item.tokenId).to.equal(indx)
+            //     expect(item.seller).to.equal(deployer.address)
+            //     expect(item.price).to.equal(i)
+            // }));
+        });
+        it("VERFIED: Deployment Fee(0.01 eth) total balance = "+ fromWei(deploymentFees)+" eth", async function () {
+            expect( await ethers.provider.getBalance(smartContract.address)).to.equal(deploymentFees);
+        });
+
+        it("VERFIED: Audio NFT data fields are valid", async function () {
+            // get each item in from the marketPlace array then check validity of fields
+            // await Promise.all(prices.map(async (i, indx) => {
+            //     const item = await nftM.marketItems(indx)
+            //     expect(item.tokenId).to.equal(indx)
+            //     expect(item.seller).to.equal(deployer.address)
+            //     expect(item.price).to.equal(i)
+            // }));
         });
 
     })
