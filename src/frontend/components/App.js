594e47bcaa09fe81ecb9da8c5b76bbd37f780d7d
Ed Chin <edgardochin@gmail.com>
2022-04-22 14:34:47 -0500

    setup initial react-routes

--- src/frontend/components/App.js
+++ src/frontend/components/App.js
@@ -1,3 +1,5 @@
+/* https://youtu.be/Q_cxytZZdnc?t=4469 */
+
 import { useState } from "react";
 import { ethers } from "ethers";
 import { 
@@ -11,7 +13,8 @@ import {
     NavBar,
     Nav,
     Button,
-    Container
+    Container,
+    Navbar
  } from "react-bootstrap";
 
 import logo from "logo.png";
@@ -24,9 +27,55 @@ function App() {
       <BrowserRouter>
         <div className="App">
           <>
-            
+            <NavBar expand="lg" bg="secondary" variant="dark">
+              <Container>
+                <Navbar.Brand href="https://github.com/ed-chin-git">
+                  <img src={logo} width="40" height="40" className="" alt="" />
+                  &nbsp; Agora
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                  <Nav className="me-auto">
+                    <Nav.Link as={Link} to="/">Home</Nav.Link>
+                    <Nav.Link as={Link} to="/my-tokens">My Tokens</Nav.Link>
+                    <Nav.Link as={Link} to="/my-resales">My Resales</Nav.Link>
+                  </Nav>
+                  <Nav>
+                    {account ? (
+                      <Nav.Link
+                        href={"https://etherscan.io/address/${account}"}
+                        target='_blank'
+                        rel='noopener noreferrer'
+                        className="button nav-button btn-sm mx-4"> 
+                        <Button variant="outline-light">
+                          {account.slice(0,5) + '...' + account.slice(38,42)}
+                        </Button>
+                      </Nav.Link>
+                    ) : ( 
+                      <Button onClick={web3Handler} variant="outline-light">
+                        Connect Wallet
+                      </Button> 
+                    )}
+                  </Nav> 
+                </Navbar.Collapse>
+              </Container>
+            </NavBar>
           </>
+          <div>
+            {loading ? (
+              <div style={{ display:'flex', justifyContent:'center', alignItems:'center', minHeight:'80vh'}}>
+                  <Spinner animation="border" style={{ display:'flex'}} />
+                  <p className="mx-3 my-0">Awaiting Metamask connection... </p>
+              </div>
+            ) : ( 
+              <Routes>
+                <Route path='/' />
+                <Route path='/my-tokens' />
+                <Route path='/my-resales' />
+              </Routes>
+            )}
+          </div>
         </div>
       </BrowserRouter>
-    )
+    );
 }
