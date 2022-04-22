2eb75a85eb6272939b8e238f202f3ed7384ad16c
Ed Chin <edgardochin@gmail.com>
2022-04-22 11:44:48 -0500

    update gitignore

--- src/frontend/components/App.js
+++ src/frontend/components/App.js
@@ -1,41 +1,32 @@
+import { useState } from "react";
+import { ethers } from "ethers";
+import { 
+    Link,
+    BrowserRouter,
+    Routes,
+    Route
+ } from "react-router-dom";
+import { 
+    Spinner,
+    NavBar,
+    Nav,
+    Button,
+    Container
+ } from "react-bootstrap";
 
-import logo from './logo.png';
-import './App.css';
- 
+import logo from "logo.png";
+import "./App.css ";
+
 function App() {
-  return (
-    <div>
-      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-        <a
-          className="navbar-brand col-sm-3 col-md-2 ms-3"
-          href="http://www.github.com/ed-chin-git"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Agora
-        </a>
-      </nav>
-      <div className="container-fluid mt-5">
-        <div className="row">
-          <main role="main" className="col-lg-12 d-flex text-center">
-            <div className="content mx-auto mt-5">
-              <a
-                href="http://www.github.com/ed-chin-git"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img src={logo} className="App-logo" alt="logo"/>
-              </a>
-              <h1 className= "mt-5">NFT Market Place</h1>
-              <p>
-                Edit <code>src/frontend/components/App.js</code> and save to reload.
-              </p>
-            </div>
-          </main>
+    const [loading, setLoading] = useState(true)
+    const [account, setAccount] = useState(null)
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <>
+            
+          </>
         </div>
-      </div>
-    </div>
-  );
+      </BrowserRouter>
+    )
 }
-
-export default App;
